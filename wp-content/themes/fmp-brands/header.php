1cac165a88c0bd0cd847d7a06cebb3456d12a7e0
Edd Twilbeck <etwilbeck@marketingincolor.com>
2019-08-14 08:53:56 -0500

    gtm and ad tracking codes in header and footer

--- wp-content/themes/fmp-brands/header.php
+++ wp-content/themes/fmp-brands/header.php
@@ -42,10 +42,31 @@ $custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' );
 
 		gtag('config', 'UA-145389082-1');
 		</script>
+
+		<!-- Global site tag (gtag.js) - Google Ads: 718117551 -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=AW-718117551"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'AW-718117551');
+		</script>
+
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-PCPCWN5');</script>
+		<!-- End Google Tag Manager -->
 	</head>
 			
 	<body <?php body_class(); ?>>
-
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCPCWN5"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 		<div class="off-canvas-wrapper">
 			
 			<!-- Load off-canvas container. Feel free to remove if not using. -->			
